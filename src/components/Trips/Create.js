c244c662c922838c513d6109f2d98efc976a415f
MARIAM ALRASDAN <mariam.alrashdan97@gmail.com>
2023-07-25 11:41:03 +0300

    almost done with get and create trips

---
+++ src/components/Trips/Create.js
@@ -0,0 +1,46 @@
+import { StyleSheet, TextInput, View } from "react-native";
+
+const Create = ({ data, setData }) => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Title"
+        placeholderTextColor="#A9A9A9"
+        value={data.title}
+        onChangeText={(value) => setData({ ...data, title: value })}
+      />
+      <TextInput
+        style={[styles.input, styles.textArea]}
+        placeholder="Description"
+        placeholderTextColor="#A9A9A9"
+        value={data.description}
+        onChangeText={(value) => setData({ ...data, description: value })}
+        multiline
+      />
+    </View>
+  );
+};
+
+export default Create;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  input: {
+    height: 45,
+    borderColor: "#D3D3D3",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 15,
+    padding: 10,
+    fontSize: 18,
+    color: "#333",
+    backgroundColor: "#F8F8F8",
+  },
+  textArea: {
+    height: 100,
+    textAlignVertical: "top",
+  },
+});
--- src/components/Trips/TripCard.js
+++ src/components/Trips/TripCard.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
 
-const TripCard = () => {
+const TripCard = ({ trip }) => {
   return (
-    <View>
-      <Text>TripCard</Text>
+    <View style={styles.card}>
+      <Image source={{ uri: trip.tripImage }} style={styles.image} />
+      <Text style={styles.title}>{trip.title}</Text>
+      <Text style={styles.description}>{trip.description}</Text>
     </View>
   );
 };
 
-export default TripCard;
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#fff",
+    margin: 20,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  image: {
+    width: "88%",
+    height: 225,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: "gray",
+    marginBottom: 10,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default TripCard;
+//{ uri: trip.imageUri }
