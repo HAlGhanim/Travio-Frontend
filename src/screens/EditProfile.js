a1ed4205080ee144eb1c837ba5931bcbe9c5bd51
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-07-29 21:56:46 +0300

    UserProfile

---
+++ src/screens/EditProfile.js
@@ -0,0 +1,122 @@
+import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
+import { updateProfile } from "../apis/auth";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { BASE_URL } from "../apis";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import ProfileNavigations from "../navigation/ProfileNavigator";
+
+const EditProfile = ({ navigation }) => {
+  const { user, setUser } = useContext(UserContext);
+  const queryClient = useQueryClient();
+
+  const [profileData, setProfileData] = useState({
+    username: user?.username || "",
+    bio: user?.bio || "",
+    image: user?.image || null,
+  });
+
+  const { mutate: updateProfileFun } = useMutation({
+    mutationFn: (data) => {
+      return updateProfile(user._id, data);
+    },
+    onSuccess: (data) => {
+      // Update the user context with the updated data
+      setUser({ ...user, ...data });
+      // Invalidate and refetch the user profile data
+      queryClient.invalidateQueries(["profile"]);
+      navigation.goBack(); // Navigate back to the MyProfile screen
+    },
+    onError: (e) => {
+      console.log(e);
+    },
+  });
+
+  const handleUpdateProfile = () => {
+    updateProfileFun(profileData);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: profileData.image
+              ? `${BASE_URL}/${profileData.image}`
+              : "https://as2.ftcdn.net/v2/jpg/02/15/84/43/1000_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg",
+          }}
+        />
+
+        <View style={styles.info}>
+          <TextInput
+            style={styles.username}
+            value={profileData.username}
+            onChangeText={(text) =>
+              setProfileData({ ...profileData, username: text })
+            }
+            placeholder="Username"
+          />
+          {/* Add more fields here for other profile information */}
+        </View>
+      </View>
+
+      <TextInput
+        style={styles.bio}
+        value={profileData.bio}
+        onChangeText={(text) => setProfileData({ ...profileData, bio: text })}
+        placeholder="Bio"
+        multiline
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Update Profile"
+          onPress={handleUpdateProfile}
+          color="darkblue"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    borderBottomColor: "#000",
+  },
+  header: {
+    marginTop: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 20,
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  info: {
+    marginLeft: 20,
+  },
+  username: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#999",
+  },
+  bio: {
+    padding: 20,
+    fontSize: 16,
+    color: "#333",
+  },
+  buttonContainer: {
+    marginTop: 20,
+    borderRadius: 7,
+    overflow: "hidden",
+  },
+});
+
+export default EditProfile;
--- src/screens/MyProfile.js
+++ src/screens/MyProfile.js
@@ -1,6 +1,5 @@
 import {
   Button,
-  FlatList,
   Image,
   SafeAreaView,
   ScrollView,
@@ -13,22 +12,23 @@ import React, { useContext, useEffect } from "react";
 import UserContext from "../context/UserContext";
 import { removeToken } from "../apis/auth/storage";
 import { profile } from "../apis/auth";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { BASE_URL } from "../apis";
-import TripCard from "../components/Trips/TripCard";
 import UserTrips from "../components/Trips/UserTrips";
+import ROUTES from "../navigation";
+import ProfileNavigation from "../navigation/ProfileNavigator";
+
 const MyProfile = ({ navigation }) => {
   const { user, setUser } = useContext(UserContext);
 
-  console.log(user);
   const handleLogout = () => {
     setUser(false);
     removeToken();
   };
-  //pass the userid from the the card created by the user
+
   const {
-    data: profileFun, //to get the user
+    data: profileFun,
     isLoading,
     refetch,
     isError,
@@ -37,19 +37,11 @@ const MyProfile = ({ navigation }) => {
     queryFn: () => profile(user._id),
   });
 
-  console.log(profileFun?.trips);
-  //should be moved to naivigation
-
   useEffect(() => {
     if (user) {
       navigation.setOptions({
         headerRight: () => (
-          <TouchableOpacity
-            style={{ marginLeft: 15 }}
-            onPress={() => {
-              handleLogout();
-            }}
-          >
+          <TouchableOpacity style={{ marginLeft: 15 }} onPress={handleLogout}>
             <MaterialCommunityIcons name="logout" size={24} color="black" />
           </TouchableOpacity>
         ),
@@ -60,6 +52,10 @@ const MyProfile = ({ navigation }) => {
       });
     }
   }, [navigation, user]);
+
+  const updateProfile = () => {
+    navigation.navigate(ROUTES.HEDERROUTES.UPDATEPROFILE);
+  };
 
   return (
     <View style={styles.container}>
@@ -72,15 +68,17 @@ const MyProfile = ({ navigation }) => {
               : `${BASE_URL}/${profileFun?.image}`,
           }}
         />
-
         <View style={styles.info}>
           <Text style={styles.username}>@{profileFun?.username}</Text>
-          <Button title="Edit Profile" onPress={() => {}} />
+          <Button
+            title="Edit Profile"
+            onPress={updateProfile}
+            color="darkblue"
+          />
         </View>
       </View>
 
       <Text style={styles.bio}>{profileFun?.bio}</Text>
-      {/* <userTrips trips={profileFun?.trips} /> */}
       <SafeAreaView style={styles.view}>
         <UserTrips user={user} />
       </SafeAreaView>
@@ -119,7 +117,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#999",
   },
-
   bio: {
     padding: 20,
     fontSize: 16,
