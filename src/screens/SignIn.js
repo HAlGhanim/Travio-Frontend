f65b80869d6e728d0195ee61d09133ddf424475e
HAlGhanim <imzaji98@gmail.com>
2023-07-30 18:34:49 +0300

    final2

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -15,6 +15,7 @@ import UserContext from "../context/UserContext";
 import { signIn } from "../apis/auth";
 import { saveToken } from "../apis/auth/storage";
 import ROUTES from "../navigation/index";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const SignIn = () => {
   const [userInfo, setUserInfo] = useState({});
@@ -46,6 +47,7 @@ const SignIn = () => {
 
   return (
     <View style={styles.container}>
+      <View style={[styles.gradient, { backgroundColor: "#e6f7ff" }]} />
       <Formik
         initialValues={{ username: "", password: "" }}
         validationSchema={validationSchema}
@@ -56,30 +58,41 @@ const SignIn = () => {
       >
         {({ handleChange, handleSubmit, values, errors, touched }) => (
           <>
-            <TextInput
-              placeholder="Username"
-              onChangeText={handleChange("username")}
-              value={values.username}
-              style={styles.input}
-              onFocus={() => {
-                setErrorText("");
-              }}
-            />
-
+            <View style={styles.inputContainer}>
+              <Icon
+                name="user"
+                size={20}
+                color="#7f8c8d"
+                style={styles.inputIcon}
+              />
+              <TextInput
+                placeholder="Username"
+                onChangeText={handleChange("username")}
+                value={values.username}
+                style={styles.input}
+                onFocus={() => setErrorText("")}
+              />
+            </View>
             {touched.username && errors.username && (
               <Text style={styles.error}>{errors.username}</Text>
             )}
 
-            <TextInput
-              placeholder="Password"
-              onChangeText={handleChange("password")}
-              value={values.password}
-              secureTextEntry
-              style={styles.input}
-              onFocus={() => {
-                setErrorText("");
-              }}
-            />
+            <View style={styles.inputContainer}>
+              <Icon
+                name="lock"
+                size={20}
+                color="#7f8c8d"
+                style={styles.inputIcon}
+              />
+              <TextInput
+                placeholder="Password"
+                onChangeText={handleChange("password")}
+                value={values.password}
+                secureTextEntry
+                style={styles.input}
+                onFocus={() => setErrorText("")}
+              />
+            </View>
 
             {touched.password && errors.password && (
               <Text style={styles.error}>{errors.password}</Text>
@@ -92,15 +105,13 @@ const SignIn = () => {
               onPress={handleSubmit}
               disabled={isLoading}
             >
-              <Text style={styles.buttonText}>Sign in</Text>
+              <Text style={styles.buttonText}>Sign In</Text>
             </TouchableOpacity>
+
             <View style={styles.signUpFlex}>
               <Text style={styles.signUpText2}>Don't Have an account?</Text>
-              <TouchableOpacity
-                style={styles.signUpText}
-                onPress={handleSignup}
-              >
-                <Text style={styles.signUpText}> Sign up.</Text>
+              <TouchableOpacity onPress={handleSignup}>
+                <Text style={styles.signUpText}> Sign up</Text>
               </TouchableOpacity>
             </View>
           </>
@@ -112,51 +123,83 @@ const SignIn = () => {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    height: "100%",
-    backgroundColor: "#fff",
-    padding: 20,
+    backgroundColor: "#f0f9ff",
+    padding: 30,
+  },
+  // gradient: {
+  //   position: "absolute",
+  //   top: 0,
+  //   left: 0,
+  //   right: 0,
+  //   bottom: 0,
+  // },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#e6e6e6",
+    borderRadius: 25,
+    marginBottom: 20,
+    width: "100%",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    elevation: 5,
   },
   input: {
-    backgroundColor: "#F2F2F2",
-    padding: 15,
-    borderRadius: 10,
-    marginBottom: 15,
-    width: "100%",
+    flex: 1,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
     fontSize: 16,
   },
+  inputIcon: {
+    marginLeft: 20,
+    marginRight: 10,
+  },
   error: {
-    color: "red",
-    marginBottom: 10,
+    color: "#c0392b",
+    marginBottom: 15,
     width: "100%",
     textAlign: "center",
+    fontSize: 14,
+    fontWeight: "600",
   },
   button: {
-    backgroundColor: "darkblue",
-    padding: 15,
-    borderRadius: 10,
-    marginTop: 20,
-    width: "100%",
+    backgroundColor: "#3498db",
+    paddingVertical: 12,
+    borderRadius: 30,
+    width: 120,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    elevation: 5,
+    transform: [{ scale: 1.02 }],
   },
   buttonText: {
     color: "white",
     textAlign: "center",
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: "bold",
   },
   signUpText: {
-    color: "#007BFF",
+    color: "#3498db",
     textAlign: "center",
-    marginLeft: 5,
+    marginLeft: 10,
+    textDecorationLine: "underline",
+    fontWeight: "600",
   },
   signUpText2: {
-    color: "gray",
+    color: "#7f8c8d",
     textAlign: "center",
   },
   signUpFlex: {
     flexDirection: "row",
-    marginTop: 10,
+    marginTop: 20,
+    alignItems: "center",
   },
 });
 
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,3 +1,4 @@
+import React, { useContext, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,17 +7,17 @@ import {
   View,
   Image,
 } from "react-native";
-import React, { useContext, useState } from "react";
-import * as ImagePicker from "expo-image-picker";
-import { useMutation } from "@tanstack/react-query";
-import { signUp } from "../apis/auth";
-import { saveToken } from "../apis/auth/storage";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { useNavigation } from "@react-navigation/native";
-import ROUTES from "../navigation";
-import UserContext from "../context/UserContext";
+import { useMutation } from "@tanstack/react-query";
 import jwt_decode from "jwt-decode";
+import UserContext from "../context/UserContext";
+import { signUp } from "../apis/auth";
+import { saveToken } from "../apis/auth/storage";
+import ROUTES from "../navigation/index";
+import Icon from "react-native-vector-icons/FontAwesome";
+import * as ImagePicker from "expo-image-picker";
 
 const SignUp = () => {
   const [image, setImage] = useState(null);
@@ -40,6 +41,7 @@ const SignUp = () => {
     },
   });
 
+  // Function to pick an image from the device's gallery
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -48,11 +50,12 @@ const SignUp = () => {
       quality: 1,
     });
 
-    if (!result.canceled) {
+    if (!result.cancelled) {
       setImage(result.uri);
     }
   };
 
+  // Form validation schema
   const validationSchema = Yup.object().shape({
     username: Yup.string().required("Username is required"),
     email: Yup.string()
@@ -61,23 +64,21 @@ const SignUp = () => {
     password: Yup.string()
       .required("Password is required")
       .matches(/.{8,}$/, "Password must be at least 8 characters long.")
-      ?.matches(/\d/, "Password must contain a number.")
-      ?.matches(/[A-Z]/, "Password must contain an uppercase letter.")
-      ?.matches(/[a-z]/, "Password must contain a lowercase letter.")
-      ?.matches(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain a symbol."),
+      .matches(/\d/, "Password must contain a number.")
+      .matches(/[A-Z]/, "Password must contain an uppercase letter.")
+      .matches(/[a-z]/, "Password must contain a lowercase letter.")
+      .matches(/[!@#$%^&*(),.?":{}|<>]/, "Password must contain a symbol."),
     passwordConfirmation: Yup.string()
       .required("Confirmation password is required")
       .oneOf([Yup.ref("password"), null], "Passwords must match"),
   });
 
+  const handleSignin = () => {
+    navigation.navigate(ROUTES.AUTHROUTES.SIGNIN);
+  };
+
   return (
-    <View
-      style={{
-        alignItems: "center",
-        height: 500,
-        justifyContent: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Formik
         initialValues={{
           username: "",
@@ -93,68 +94,94 @@ const SignUp = () => {
       >
         {({ handleChange, handleSubmit, values, errors, touched }) => (
           <>
+            {/* Image picker */}
             <TouchableOpacity onPress={pickImage}>
-              <View
-                style={{
-                  width: 100,
-                  height: 100,
-                  backgroundColor: "gray",
-                  borderRadius: 50,
-                  overflow: "hidden",
-                  marginBottom: 20,
-                }}
-              >
-                {image && (
-                  <Image
-                    source={{ uri: image }}
-                    style={{ width: "100%", height: "100%" }}
-                  />
+              <View style={styles.imageContainer}>
+                {/* Display selected image or user icon */}
+                {image ? (
+                  <Image source={{ uri: image }} style={styles.image} />
+                ) : (
+                  <Icon name="user" size={50} color="#7f8c8d" />
                 )}
               </View>
             </TouchableOpacity>
 
-            <TextInput
-              placeholder="Username"
-              onChangeText={handleChange("username")}
-              value={values.username}
-            />
+            {/* Username input */}
+            <View style={styles.inputContainer}>
+              <Icon name="user" size={20} color="#7f8c8d" style={styles.icon} />
+              <TextInput
+                placeholder="Username"
+                onChangeText={handleChange("username")}
+                value={values.username}
+                style={styles.input}
+              />
+            </View>
             {touched.username && errors.username && (
               <Text style={styles.error}>{errors.username}</Text>
             )}
-            <TextInput
-              placeholder="Email"
-              onChangeText={handleChange("email")}
-              value={values.email}
-            />
+
+            {/* Email input */}
+            <View style={styles.inputContainer}>
+              <Icon
+                name="envelope"
+                size={20}
+                color="#7f8c8d"
+                style={styles.icon}
+              />
+              <TextInput
+                placeholder="Email"
+                onChangeText={handleChange("email")}
+                value={values.email}
+                style={styles.input}
+              />
+            </View>
             {touched.email && errors.email && (
               <Text style={styles.error}>{errors.email}</Text>
             )}
 
-            <TextInput
-              placeholder="Password"
-              onChangeText={handleChange("password")}
-              value={values.password}
-              secureTextEntry
-            />
+            {/* Password input */}
+            <View style={styles.inputContainer}>
+              <Icon name="lock" size={20} color="#7f8c8d" style={styles.icon} />
+              <TextInput
+                placeholder="Password"
+                onChangeText={handleChange("password")}
+                value={values.password}
+                secureTextEntry
+                style={styles.input}
+              />
+            </View>
             {touched.password && errors.password && (
               <Text style={styles.error}>{errors.password}</Text>
             )}
 
-            <TextInput
-              placeholder="Password Confirmation"
-              onChangeText={handleChange("passwordConfirmation")}
-              value={values.passwordConfirmation}
-              secureTextEntry
-            />
+            {/* Password confirmation input */}
+            <View style={styles.inputContainer}>
+              <Icon name="lock" size={20} color="#7f8c8d" style={styles.icon} />
+              <TextInput
+                placeholder="Password Confirmation"
+                onChangeText={handleChange("passwordConfirmation")}
+                value={values.passwordConfirmation}
+                secureTextEntry
+                style={styles.input}
+              />
+            </View>
             {touched.passwordConfirmation && errors.passwordConfirmation && (
               <Text style={styles.error}>{errors.passwordConfirmation}</Text>
             )}
 
+            {/* Display error message */}
             {errorText ? <Text style={styles.error}>{errorText}</Text> : null}
 
+            {/* Signup button */}
             <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-              <Text style={styles.buttonText}>Signup</Text>
+              <Text style={styles.buttonText}>Sign Up</Text>
             </TouchableOpacity>
+            <View style={styles.signUpFlex}>
+              <Text style={styles.signUpText2}>Do you have an account?</Text>
+              <TouchableOpacity onPress={handleSignin}>
+                <Text style={styles.signUpText}> Sign in</Text>
+              </TouchableOpacity>
+            </View>
           </>
         )}
       </Formik>
@@ -167,8 +194,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#fff",
-    padding: 20,
+    backgroundColor: "#f0f9ff",
+    padding: 30,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
   },
   imageContainer: {
     width: 100,
@@ -176,41 +208,73 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     overflow: "hidden",
     marginBottom: 20,
-    backgroundColor: "#e0e0e0", // this is a light grey, you can replace it with any color you like
+    backgroundColor: "#e6e6e6", // Background color for the image container
     justifyContent: "center",
     alignItems: "center",
   },
   image: {
     width: "100%",
     height: "100%",
+    borderRadius: 50,
   },
   input: {
     width: "100%",
     height: 50,
-    backgroundColor: "#F2F2F2",
-    borderRadius: 10,
-    marginBottom: 15,
-    paddingHorizontal: 15,
+    backgroundColor: "#e6e6e6", // Background color for the input fields
+    borderRadius: 25,
+    marginBottom: 20,
+    paddingHorizontal: 20,
     fontSize: 16,
   },
   error: {
-    color: "red",
+    color: "#c0392b",
     marginBottom: 15,
-    alignSelf: "flex-start",
+    width: "100%",
+    textAlign: "center",
+    fontSize: 14,
+    fontWeight: "600",
   },
   button: {
-    width: "80%",
-    height: 50,
-    backgroundColor: "darkblue",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    marginTop: 20,
+    backgroundColor: "#3498db",
+    paddingVertical: 12,
+    borderRadius: 30,
+    width: 120,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    elevation: 5,
+    transform: [{ scale: 1.02 }],
   },
   buttonText: {
     color: "white",
-    fontSize: 16,
+    textAlign: "center",
+    fontSize: 18,
     fontWeight: "bold",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#e6e6e6", // Background color for the input fields
+    borderRadius: 25,
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    fontSize: 16,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  signUpFlex: {
+    flexDirection: "row",
+    marginTop: 20,
+    alignItems: "center",
+  },
+  signUpText: {
+    color: "#3498db",
+    textAlign: "center",
+    marginLeft: 10,
+    textDecorationLine: "underline",
+    fontWeight: "600",
   },
 });
 
